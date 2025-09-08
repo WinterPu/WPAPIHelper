2a13b382e36f4c7e670c7a8f07bad4fedd33c691
WinterPu <striveant@outlook.com>
2025-09-08 14:57:17 +0800

    Update feishu_helper.py

--- feishu_helper.py
+++ feishu_helper.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 
+import requests
+
 ## Load .env file
 load_dotenv()
 
@@ -9,3 +11,13 @@ app_secret = os.getenv('FEISHU_APP_SECRET')
 
 print(f"FEISHU_APP_ID: {app_id}")
 print(f"FEISHU_APP_SECRET: {app_secret}")
+
+
+url= "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/" 
+#应用凭证里的 app id 和 app secret  
+post_data = {"app_id": app_id, "app_secret": app_secret}
+r = requests.post(url, data=post_data)
+tat = r.json()["tenant_access_token"] 
+print(f"TAT: {tat}")
+
+
